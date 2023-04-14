9dd9c0334e352b7cdf4abfef31e5579c831bfa56
liyy0 <yuyanli@bu.edu>
2023-04-13 20:53:32 -0400

    update port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8080;
 const path = require('path');
 
 app.use(cors());
@@ -41,4 +41,4 @@ app.use(express.static(path.join(__dirname,"client", 'build')));
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port} .`);
-});
+  });
